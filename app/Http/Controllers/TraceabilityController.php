56956dd449bc74063481fb2820179f48a45d7de1
Luciano Aguilar <lucianodavidaguilar21@gmail.com>
2025-06-30 21:58:06 -0300

    lang es, entities

---
+++ app/Http/Controllers/TraceabilityController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Traceability;
+use Illuminate\Http\Request;
+
+class TraceabilityController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Traceability $traceability)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Traceability $traceability)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Traceability $traceability)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Traceability $traceability)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,10 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/administracion', function () {
+        return view('admin.admin');
+    })->name('administracion');
 });
 
 require __DIR__.'/auth.php';
